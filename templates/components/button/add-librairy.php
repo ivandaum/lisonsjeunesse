e26f3c83e6276246c5173d0983e89cc6b054d80a
Ivan Daum <ivan.daum@gmail.com>
2020-05-17 20:42:42 +0200

    style(like): fix broken button on ios

--- templates/components/button/add-librairy.php
+++ templates/components/button/add-librairy.php
@@ -1,6 +1,6 @@
 <?php use \Lisonsjeunesse\Core\Utils\Svg; ?>
 <?php if(isset($id)): ?>
-<button class="button-librairy js-add-to-librairy is-relative has-width-100 is-flex is-center-y <?php if($active): ?>is-active<?php endif; ?>" data-id="<?= $id ?>">
+<button class="button-librairy js-add-to-librairy is-relative has-width-100 is-flex <?php if($active): ?>is-active<?php endif; ?>" data-id="<?= $id ?>">
     <?= Svg::print('like'); ?>
     <div class="button-librairy--wording has-width-100 has-text-left">
         <span class="is-block">Ajouter à ma bibliothèque</span>
